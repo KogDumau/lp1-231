b5a961afeed856e321b2e6b518325e71b227a1f9
Raul <raulbbrec@gmail.com>
2023-05-23 22:01:10 -0300

    Exercicios

---
+++ Semana12/Circulo.java
@@ -0,0 +1,16 @@
+package Semana12;
+
+public class Circulo {
+    public double raio;
+
+    public Circulo(double raio){
+        this.raio = raio;
+    }
+
+    public double calcularArea(){
+        return Math.PI * (raio * raio);
+    }
+    public double calcularPerimetro(){
+        return 2 * Math.PI * raio;
+    }
+}
---
+++ Semana12/Exercicios/Aquariotest.java
@@ -0,0 +1,82 @@
+package Semana12.Exercicios;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals; 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals; 
+
+public class Aquariotest {
+    
+    @Test
+    public void testIndicarValoresParaAsVariaveis(){
+        Aquario aquario = new Aquario(100.0, 50.0, 50.0);
+
+        assertEquals(100.0, aquario.comprimento);
+        assertEquals(50.0, aquario.altura);
+        assertEquals(50.0, aquario.largura);
+    }
+
+    @Test
+    public void testIndicarUmUnicoValor(){
+        Aquario aquario = new Aquario(50.0, 50.0, 50.0);
+
+        assertEquals(50.0, aquario.comprimento);
+        assertEquals(50.0, aquario.altura);
+        assertEquals(50.0, aquario.largura);
+    }
+
+    @Test
+    public void testCalcularVolume(){
+        Aquario aquario = new Aquario(100.0, 50.0, 50.0);
+
+        aquario.altura = 50.0;
+        aquario.comprimento = 100.0;
+        aquario.largura = 50.0;
+
+        double volume = aquario.calcularVolume();
+
+        assertEquals(250.0, volume);
+    }
+
+    @Test
+    public void testCalcularVolume2(){
+        Aquario aquario = new Aquario(100.0, 50.0, 0.0);
+
+        aquario.altura = 50.0;
+        aquario.comprimento = 100.0;
+        aquario.largura = 0.0;
+
+        double volume = aquario.calcularVolume();
+
+        assertEquals(0.0, volume);
+    }
+
+    @Test
+    public void testCalcularPotenciaDoTermostato(){
+        Aquario aquario = new Aquario(100.0, 50.0, 50.0);
+
+        aquario.altura = 50.0;
+        aquario.comprimento = 100.0;
+        aquario.largura = 50.0;
+        double temperaturaDesejada = 30;
+        double temperaturaAmbiente = 17;
+
+        double potencia = aquario.calcularPotenciaDoTermostato(temperaturaDesejada, temperaturaAmbiente);
+
+        assertEquals(162.5, potencia);
+    }
+
+    @Test
+    public void calcularQuantidadeLitrosDuasETr3sVezes(){
+        Aquario aquario = new Aquario(100.0, 50.0, 50.0);
+
+        aquario.altura = 50.0;
+        aquario.comprimento = 100.0;
+        aquario.largura = 50.0;
+
+        double[] litros = aquario.calcularQuantidadeLitrosFiltro();
+        double[] esperado = {500, 750};
+        assertArrayEquals(esperado, litros);
+    }
+}
